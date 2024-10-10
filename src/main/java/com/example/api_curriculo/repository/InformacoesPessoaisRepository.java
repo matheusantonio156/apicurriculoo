90d95a1eb4a50c25e5c6d71719d49adfbc1113d9
matheusantonio156 <164435308+matheusantonio156@users.noreply.github.com>
2024-10-10 12:40:13 -0300

    Update InformacoesPessoaisRepository.java

--- src/main/java/com/example/api_curriculo/repository/InformacoesPessoaisRepository.java
+++ src/main/java/com/example/api_curriculo/repository/InformacoesPessoaisRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.api_curriculo.entity.InformacoesPessoais;
 
 public interface InformacoesPessoaisRepository extends JpaRepository<InformacoesPessoais, Long> {
-    // Você pode adicionar métodos personalizados aqui, se necessário
 }

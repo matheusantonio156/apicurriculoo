fe641e44566176fb2f3733866a698af9cdae6578
matheusantonio156 <164435308+matheusantonio156@users.noreply.github.com>
2024-10-10 15:18:01 -0300

    Update InformacoesPessoais.java

--- src/main/java/com/example/api_curriculo/entity/InformacoesPessoais.java
+++ src/main/java/com/example/api_curriculo/entity/InformacoesPessoais.java
@@ -7,16 +7,17 @@ import lombok.Data;
 @Entity
 @Table(name = "informacoes_pessoais")
 public class InformacoesPessoais {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String endereco;   // Endereço da pessoa
-    private String nome;       // Nome da pessoa
-    private String telefone;   // Telefone da pessoa
-    private String email;      // Email da pessoa
-    private String resumo;     // Resumo sobre a pessoa
-    private int idade;         // Idade da pessoa
-    private String curso;      // Curso que a pessoa está fazendo
+    private String nome;
+    private String email;
+    private String endereco;
+    private String resumo;
+    private String idade;
+    private String curso;
+
+    // Adicione outros campos conforme necessário
 }

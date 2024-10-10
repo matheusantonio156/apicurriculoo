4017dfc330566b01ce4707286d528730690d056a
matheusantonio156 <164435308+matheusantonio156@users.noreply.github.com>
2024-10-10 14:56:43 -0300

    Update InformacoesPessoais.java

--- src/main/java/com/example/api_curriculo/entity/InformacoesPessoais.java
+++ src/main/java/com/example/api_curriculo/entity/InformacoesPessoais.java
@@ -5,17 +5,17 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "informacoes_pessoais")
+@Table(name = "informacoes_pessoais") // Certifique-se de que a tabela no banco de dados tenha o mesmo nome
 public class InformacoesPessoais {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome;       // Nome completo
-    private String email;      // Email
-    private String endereco;   // Endereço
-    private String idade;      // Idade
-    private String curso;      // Curso
-    private String resumo;     // Resumo pessoal
+    private String nome;
+    private String email;
+    private String endereco;
+    private String resumo;
+    private int idade; // Utilize int para idade
+    private String curso;
 }
